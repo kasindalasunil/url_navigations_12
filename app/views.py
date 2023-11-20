96ab500bce24804f38febdd4f09b7814fc808e42
kasindalasunil <kasindalasunil316@gmail.com>
2023-11-20 23:36:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rohit(request):
+    return render(request,'rohit.html')
+
+def virat(request):
+    return render(request,'virat.html')
+
+def shami(request):
+    return render(request,'shami.html')
+
+
+
